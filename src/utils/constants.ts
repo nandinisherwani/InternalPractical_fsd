deba23977ca3a4d4f241444ea0ed7b4d76fa02fc
nandinisherwani <nandinisherwani@gmail.com>
2025-09-19 10:23:57 +0530

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+
+export const TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlqa2x2eGhycHdvYmJkbXZzam95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTQ3OTUsImV4cCI6MjA3MzgzMDc5NX0.FHhBPEJV73dMpXra0IFVN6Up_fc_nrfnQeFcH5j0R8U';
+
+export const MOVIE_CATEGORIES = {
+  trending: '/trending/all/day',
+  topRated: '/movie/top_rated',
+  action: '/discover/movie?with_genres=28',
+  tvShows: '/discover/tv'
+};
